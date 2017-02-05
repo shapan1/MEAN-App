ff42b385b8dce72f84fa4925c46da7d3c8d35a5a
Shapan Dashore <Shapan Dashore>
2017-02-04 21:17:51 -0500

    connection to MongoLab made for prod environment

--- server.js
+++ server.js
@@ -24,8 +24,15 @@ app.use(stylus.middleware(
   }
 ));
 app.use(express.static(__dirname + '/public'));
+/*
+for switching between dev and prod environments.
+ */
+if(env === 'development'){
+    mongoose.connect('mongodb://localhost/multivision');
+} else {
+    mongoose.connect('mongodb://shapan:shapan1234@ds143449.mlab.com:43449/multivision');
+}
 
-mongoose.connect('mongodb://localhost/multivision');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
@@ -48,6 +55,6 @@ app.get('*', function(req, res) {
   });
 });
 
-var port = 3030;
+var port = process.env.port || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
